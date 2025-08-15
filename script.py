d6adba994778c967f19d8de236d84312f6c768de
Sazid <sazidshihab01@gmail.com>
2025-08-16 03:16:17 +0600

    Create script.py

---
+++ script.py
@@ -0,0 +1,16 @@
+import pymysql
+import pandas as pd
+
+# EC2 reverse tunnel points to your Mac MySQL
+conn = pymysql.connect(
+    host='13.60.117.189',  # EC2 public IP
+    port=33306,            # Reverse tunnel port
+    user='sazid',
+    password='123456789abc',
+    database='erpboss'
+)
+
+df = pd.read_sql("SELECT * FROM stock", conn)
+print(df.head())  # Output will appear in GitHub Actions log
+
+conn.close()
