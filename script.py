e5131dc0f6974fc18a33b7a32a3ce63f72bd07af
Sazid <sazidshihab01@gmail.com>
2025-08-16 03:58:32 +0600

    Update script.py

--- script.py
+++ script.py
@@ -1,12 +1,15 @@
 
-
 import pymysql
+import pandas as pd
 
 conn = pymysql.connect(
-    host='13.60.117.189',
-    port=33306,
-    user='MYSQL_USER',
+    host='13.60.117.189',  # public IP of your EC2
+    port=33306,            # remote port forwarded to your Mac
+    user='MYSQL_USER',     # MySQL user on your Mac
     password='MYSQL_PASSWORD',
     database='erpboss'
 )
 
+df = pd.read_sql("SELECT * FROM stock", conn)
+print(df.head())
+conn.close()
