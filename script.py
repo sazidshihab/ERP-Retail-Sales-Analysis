0ad279731632d7522113c9c9bdbcb0b375a16924
Sazid <sazidshihab01@gmail.com>
2025-08-18 04:21:01 +0600

    Update script.py

--- script.py
+++ script.py
@@ -26,3 +26,34 @@ df = pd.read_sql(query, conn)
 print(df.head())
 
 conn.close()
+
+
+import gspread
+from google.oauth2.service_account import Credentials
+import pandas as pd
+
+
+# Authenticate
+scopes = ["https://www.googleapis.com/auth/spreadsheets",
+          "https://www.googleapis.com/auth/drive"]
+
+
+
+creds_json = os.getenv("SERVICE_ACNT_CRED")
+creds_dict = json.loads(creds_json)
+
+creds = Credentials.from_service_account_info(
+    creds_dict,  scopes=scopes
+)
+
+client = gspread.authorize(creds)
+sheet = client.open("test_auto").sheet1
+
+# Convert DataFrame to list of lists
+rows_to_append = df.values.tolist()
+
+# Append all rows at once
+sheet.append_rows(rows_to_append, value_input_option='USER_ENTERED')
+print("Data appended successfully!")
+
+
