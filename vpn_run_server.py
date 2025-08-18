8b7bf081966db8a8b6688df7eecc0f239f27e29b
Sazid <sazidshihab01@gmail.com>
2025-08-19 01:46:31 +0600

    Update vpn_run_server.py

--- vpn_run_server.py
+++ vpn_run_server.py
@@ -9,7 +9,31 @@ conn = pymysql.connect(
     database='erpboss'
 )
 
-df = pd.read_sql("SELECT * FROM stock LIMIT 10;", conn)
+df = pd.read_sql("SELECT * FROM stock LIMIT 50;", conn)
 print(df)
 
 conn.close()
+
+
+# Authenticate
+scopes = ["https://www.googleapis.com/auth/spreadsheets",
+          "https://www.googleapis.com/auth/drive"]
+
+
+
+creds_json = os.getenv("SERVICE_ACNT_CRED")
+creds_dict = json.loads(creds_json)
+
+creds = Credentials.from_service_account_info(
+    creds_dict,  scopes=scopes
+)
+
+client = gspread.authorize(creds)
+sheet = client.open("test_auto").sheet1
+
+# Convert DataFrame to list of lists
+rows_to_append = df.values.tolist()
+
+# Append all rows at once
+sheet.append_rows(rows_to_append, value_input_option='USER_ENTERED')
+print("Data appended successfully!")
